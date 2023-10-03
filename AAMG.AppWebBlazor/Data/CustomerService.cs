8a7aca35bf664334aec5235652aced37d94ece84
adonismolinao3 <adonismolinao3@outlook.com>
2023-10-03 15:00:22 -0600

    INICIO BLAZOR

---
+++ AAMG.AppWebBlazor/Data/CustomerService.cs
@@ -0,0 +1,75 @@
+﻿using AAMG.DTOs.CustomerDTOs;
+
+namespace AAMG.AppWebBlazor.Data
+{
+    public class CustomerService
+    {
+        readonly HttpClient _httpClientAAMGAPI;
+        public CustomerService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientAAMGAPI = httpClientFactory.CreateClient("KJMCAPI");
+        }
+
+        public async Task<SearchResultCustomerDTO> Search(SearchQueryCustomerDTO queryCustomerDTO)
+        {
+            var response = await _httpClientAAMGAPI.PostAsJsonAsync("/customer/search", queryCustomerDTO);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<SearchResultCustomerDTO>();
+                return result ?? new SearchResultCustomerDTO();
+            }
+            return new SearchResultCustomerDTO();
+        }
+
+        public async Task<GetIdResultCustomerDTO> GetById(int id)
+        {
+            var response = await _httpClientAAMGAPI.GetAsync("/customer/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<GetIdResultCustomerDTO>();
+                return result ?? new GetIdResultCustomerDTO();
+            }
+            return new GetIdResultCustomerDTO();
+        }
+
+        public async Task<int> Create(CreateCustomerDTO createDTO)
+        {
+            int result = 0;
+            var response = await _httpClientAAMGAPI.PostAsJsonAsync("/customer", createDTO);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                if (int.TryParse(responseBody, out result) == false)
+                    result = 0;
+            }
+            return result;
+        }
+
+        public async Task<int> Edit(EditCustomerDTO editDTO)
+        {
+            int result = 0;
+            var response = await _httpClientAAMGAPI.PutAsJsonAsync("/customer", editDTO);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                if (int.TryParse(responseBody, out result) == false)
+                    result = 0;
+            }
+            return result;
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            int result = 0;
+            var response = await _httpClientAAMGAPI.DeleteAsync("/customer/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                if (int.TryParse(responseBody, out result) == false)
+                    result = 0;
+            }
+            return result;
+        }
+    }
+}
